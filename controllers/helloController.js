824f1be7c771b138655e0f9643cf94395bc7e3fc
Somanyu <somanyu.03samal@gmail.com>
2023-05-07 17:17:16 +0530

    feat: add controller for routes

---
+++ controllers/helloController.js
@@ -0,0 +1,10 @@
+
+// Controller function for the "Hello, World!" endpoint
+const sayHello = (req, res) => {
+    res.status(200).json({ message: 'Hello, World!' });
+  };
+  
+  module.exports = {
+    sayHello
+  };
+  
--- routes/exampleRoute.js
+++ routes/exampleRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { toTitleCase } = require('../utils/stringUtils');
+const helloController = require('../controllers/helloController');
 
 // GET route
 router.get('/example', (req, res) => {
@@ -16,5 +17,8 @@ router.post('/example', (req, res) => {
     const { email, password } = req.body;
     res.json({ message: 'This is an example of a POST request' });
 });
+
+// GET route with controller
+router.get('/hello', helloController.sayHello);
 
 module.exports = router;
