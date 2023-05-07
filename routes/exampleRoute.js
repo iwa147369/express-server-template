50d95be20f525d6b1de081fbb8b328640adc6d1f
Somanyu <somanyu.03samal@gmail.com>
2023-05-07 13:31:11 +0530

    add: using string utils

--- routes/exampleRoute.js
+++ routes/exampleRoute.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const { toTitleCase } = require('../utils/stringUtils');
 
 // GET route
 router.get('/example', (req, res) => {
-    res.json({ message: 'This is an example of a GET request' });
+    const name = 'john doe';
+    const titleCaseName = toTitleCase(name);
+    console.log(titleCaseName);
+
+    res.json({ message: 'This is an example of a GET request', name: titleCaseName });
 });
 
 // POST route
