918d10a6c3c22a1c6ca4203acd9ea39f6c5e7dc0
Somanyu <somanyu.03samal@gmail.com>
2023-05-07 17:24:14 +0530

    add: service for adding two numbers

--- routes/exampleRoute.js
+++ routes/exampleRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { toTitleCase } = require('../utils/stringUtils');
 const helloController = require('../controllers/helloController');
+const calculationService = require('../services/calculationService')
 
 // GET route
 router.get('/example', (req, res) => {
@@ -20,5 +21,13 @@ router.post('/example', (req, res) => {
 
 // GET route with controller
 router.get('/hello', helloController.sayHello);
+
+// GET route with service
+router.get('/calc', (req, res) => {
+    const firstNumber = 12;
+    const secondNumber = 9;
+    const result = calculationService.addNumbers(firstNumber, secondNumber);
+    res.json({ message: 'This is an example of a GET request with service', firstNumber: firstNumber, secondNumber: secondNumber, result: result });
+})
 
 module.exports = router;
